1d5ab2d609dc95fb9ec4870912b09fb293547a0d
mikyegresl <mikyegresl@gmail.com>
2021-08-02 13:20:57 +0500

    LineChart rounded corners while more than 1 transaction type

--- app/src/main/java/uz/alex/its/beverlee/view/LineChart.java
+++ app/src/main/java/uz/alex/its/beverlee/view/LineChart.java
@@ -3,6 +3,7 @@ package uz.alex.its.beverlee.view;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.util.AttributeSet;
@@ -24,6 +25,20 @@ public class LineChart extends AppCompatSeekBar {
     private List<LineChartItem> itemList;
     private List<Paint> paintList;
     private List<RectF> rectFList;
+    private List<Path> pathList;
+
+    private static final float[] leftRoundedArr = new float[] {
+            15f, 15f,
+            0f, 0f,
+            0f, 0f,
+            15f, 15f
+    };
+    private static final float[] rightRoundedArr = new float[] {
+            0f, 0f,
+            15f, 15f,
+            15f, 15f,
+            0f, 0f
+    };
 
     public LineChart(final Context context) {
         super(context);
@@ -49,10 +64,12 @@ public class LineChart extends AppCompatSeekBar {
         if (itemList.size() > 0) {
             this.paintList = new ArrayList<>();
             this.rectFList = new ArrayList<>();
+            this.pathList = new ArrayList<>();
 
             for (int i = 0; i < itemList.size(); i++) {
                 this.paintList.add(new Paint());
                 this.rectFList.add(new RectF());
+                this.pathList.add(new Path());
             }
         }
     }
@@ -87,13 +104,29 @@ public class LineChart extends AppCompatSeekBar {
                     itemRight = lineChartWidth;
                 }
                 rectFList.get(i).set(lastPosition, (float) thumbOffset / 2, itemRight, lineChartHeight - (float) thumbOffset / 2);
-//                canvas.drawRect(rectFList.get(i), paintList.get(i));
-                canvas.drawRoundRect(rectFList.get(i), 12, 16, paintList.get(i));
+
+                if (itemList.size() == 1) {
+                    canvas.drawRoundRect(rectFList.get(i), 12, 16, paintList.get(i));
+                }
+                else if (i == 0 || i == itemList.size() - 1) {
+                    if (i == 0) {
+                        Log.i(TAG, "onDraw: i == 0");
+                        pathList.get(i).addRoundRect(rectFList.get(i), leftRoundedArr, Path.Direction.CW);
+                    }
+                    else if (i == itemList.size() - 1) {
+                        Log.i(TAG, "onDraw: i == size - 1");
+                        pathList.get(i).addRoundRect(rectFList.get(i), rightRoundedArr, Path.Direction.CW);
+                    }
+                    pathList.get(i).close();
+                    canvas.drawPath(pathList.get(i), paintList.get(i));
+                }
+                else {
+                    canvas.drawRect(rectFList.get(i), paintList.get(i));
+                }
                 lastPosition = itemRight;
             }
             super.onDraw(canvas);
         }
-
     }
 
     private static final String TAG = LineChart.class.toString();
